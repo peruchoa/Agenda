959b529ed7f1e676c91e1fa94e12d041341cfdb2
Peruchoa <40150117+peruchoa@users.noreply.github.com>
2022-12-09 02:29:23 -0400

    Hasta aqui este proyecto, no se pudo corregir.

--- app/src/main/java/com/example/agenda/vistas/Adaptador.java
+++ app/src/main/java/com/example/agenda/vistas/Adaptador.java
@@ -1,4 +1,5 @@
 package com.example.agenda.vistas;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -16,12 +17,12 @@ import com.example.agenda.R;
 
 import java.util.ArrayList;
 public class Adaptador extends RecyclerView.Adapter<Adaptador.Vista> {
-    private Context context;
-    private ArrayList<String> ids;
-    private ArrayList<String> nombres;
-    private ArrayList<String> telefonos;
-    private ArrayList<String> correos;
-    private ArrayList<String> direcciones;
+    private final Context context;
+    private final ArrayList<String> ids;
+    private final ArrayList<String> nombres;
+    private final ArrayList<String> telefonos;
+    private final ArrayList<String> correos;
+    private final ArrayList<String> direcciones;
     ConstraintLayout fila;
 
     public Adaptador(Context context, ArrayList<String> ids, ArrayList<String> nombres, ArrayList<String> telefonos, ArrayList<String> correos, ArrayList<String> direcciones) {
@@ -40,29 +41,26 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.Vista> {
         return new Vista(v);
     }
     @Override
-    public void onBindViewHolder(@NonNull Adaptador.Vista holder, int position) {
+    public void onBindViewHolder(@NonNull Adaptador.Vista holder, @SuppressLint("RecyclerView") int position) {
         holder.txtShowNombre.setText(nombres.get(position));
         holder.txtShowTelefono.setText(telefonos.get(position));
 
-        fila.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        fila.setOnClickListener(v -> {
 
-                // Cuando de click en el elemento que me envie hacia el formulario de actualizar.
+            // Cuando de click en el elemento que me envie hacia el formulario de actualizar.
 
-                //Cuando pida un 'context' se toma del metodo constructor.
-                // Luego poner el formulario a donde se desea ir.
+            //Cuando pida un 'context' se toma del metodo constructor.
+            // Luego poner el formulario a donde se desea ir.
 
-                Intent intent = new Intent(context, Actualizar.class);
+            Intent intent = new Intent(context, Actualizar.class);
 
-                intent.putExtra("id", ids.get(position));
-                intent.putExtra("nombre", nombres.get(position));
-                intent.putExtra("telefono", telefonos.get(position));
-                intent.putExtra("correo",  correos.get(position));
-                intent.putExtra("direccion", direcciones.get(position));
+            intent.putExtra("id", ids.get(position));
+            intent.putExtra("nombre", nombres.get(position));
+            intent.putExtra("telefono", telefonos.get(position));
+            intent.putExtra("correo",  correos.get(position));
+            intent.putExtra("direccion", direcciones.get(position));
 
-                context.startActivity(intent);
-            }
+            context.startActivity(intent);
         }) ;
     }
     @Override
